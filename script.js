84567a2a996581c717ff3565c3328df0af837611
Anton-Green <anton.onokhin2@gmail.com>
2024-01-11 21:46:33 -0600

    comeback to 1/9/2024

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ function showMessage() {
     alert('Message successfully sent');
 }
 
-/*
-$(document).ready(function () {
-	// Координаты курсора относительно всего документа
-	$(document).mousemove(function (event) {
-		var x = event.pageX;
-		var y = event.pageY;
-		$('#coords1').html('Координаты курсора: (' + x + '; ' + y + ')');
-	});
-});
-*/
